ef7d1fcd08791962e6cfda1cbfe2e1feafc267df
Sawera Sajid <93257845+sawera60@users.noreply.github.com>
2026-03-05 19:25:46 +0500

    added navbar of recipe app

---
+++ 4.Recipe WebApp/my-app/src/Components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaSearch } from "react-icons/fa";
+
+const Navbar = () => {
+    return (
+        <nav className='w-full h-16 bg-blue-800 text-white flex items-center justify-between px-4'>
+            <div className="left font-bold">
+                <h1>Recipe Web App</h1>
+            </div>
+            <div className="middle flex items-center gap-2">
+                <input type="text" placeholder='Search here' className='border-2 rounded-2xl px-2' />
+                <FaSearch />
+
+            </div>
+            <div className="right flex gap-5">
+                <h3>Indian</h3>
+                <h3>American</h3>
+                <h3>British</h3>
+                <h3>Chinese</h3>
+                <h3>Thai</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
